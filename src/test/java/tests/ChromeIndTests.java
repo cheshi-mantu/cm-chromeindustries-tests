19e7e2fbf4c36446c70355db8c91e0cdc25fa0f2
cheshi-mantu <cheshi.mantu@gmail.com>
2020-07-27 18:24:16 +0300

    another try

--- src/test/java/tests/ChromeIndTests.java
+++ src/test/java/tests/ChromeIndTests.java
@@ -67,7 +67,11 @@ class ChromeIndTests extends TestBase {
         });
         step("Go Vega Brief page, price should be " + checkPrice, () -> {
 //            $(byText("Vega 2.0 Transit Brief")).scrollIntoView("{behavior:\"smooth\", block: \"start\", inline: \"nearest\"}");
-            $(byText("Vega 2.0 Transit Brief")).scrollIntoView(true);
+//            $(byText("Vega 2.0 Transit Brief")).scrollIntoView(false);
+            $(byText("Vega 2.0 Transit Brief")).scrollTo();
+            if ($(".notice--hide").isDisplayed()){
+                $(".notice--hide").click();
+            }
             $(byText("Vega 2.0 Transit Brief")).click();
             String vegaTextVegaPrice = $(".product-price span").innerText();
             AttachmentsHelper.attachAsText("Current price on page", vegaTextVegaPrice);
